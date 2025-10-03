e37e170a59ad4f056b1bb5cd2a29a137ceaa4488
Nora <46890129+RainbowDashLabs@users.noreply.github.com>
2025-10-04 00:24:49 +0200

    update setup file

--- src/main/resources/database/postgresql/1/setup.sql
+++ src/main/resources/database/postgresql/1/setup.sql
@@ -3,21 +3,20 @@ CREATE TABLE forest_schema.nodes (
         CONSTRAINT nodes_pk
             PRIMARY KEY,
     world         UUID                    NOT NULL,
-    last_modified TIMESTAMP DEFAULT now() NOT NULL,
-    preset        TEXT                    NOT NULL
+    last_modified TIMESTAMP DEFAULT now() NOT NULL
 );
 
 CREATE TABLE forest_schema.fragments (
-    node_id    BIGINT  NOT NULL,
-    world      UUID    NOT NULL,
-    x          INTEGER NOT NULL,
-    y          INTEGER NOT NULL,
-    z          INTEGER NOT NULL,
-    material   TEXT    NOT NULL,
-    block_data TEXT    NOT NULL,
-    destroyed  TIMESTAMP,
-    CONSTRAINT fragments_pk
-        PRIMARY KEY (world, y, z, x)
+    node_id        BIGINT  NOT NULL,
+    world          UUID    NOT NULL,
+    x              INTEGER NOT NULL,
+    y              INTEGER NOT NULL,
+    z              INTEGER NOT NULL,
+    block_data     TEXT    NOT NULL,
+    destroyed      TIMESTAMP,
+    always_restore BOOLEAN
+        CONSTRAINT fragments_pk
+            PRIMARY KEY (world, y, z, x)
 );
 
 DROP INDEX IF EXISTS forest_schema.fragments_world_node_id_idx;
